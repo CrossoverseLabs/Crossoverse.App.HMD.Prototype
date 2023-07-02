208b257f4efc85a2b910a3dcc1be13b7cd99f905
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-07-02 17:23:53 +0900

    Add AvatarContext

--- Crossoverse.HMDApp.Quest/Assets/Crossoverse.HMDApp.Quest/Application/Main.cs
+++ Crossoverse.HMDApp.Quest/Assets/Crossoverse.HMDApp.Quest/Application/Main.cs
@@ -1,3 +1,7 @@
+using System.IO;
+using Crossoverse.Core.Domain.ResourceProvider;
+using Crossoverse.HMDApp.Quest.Context;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +9,29 @@ namespace Crossoverse.HMDApp.Quest
 {
     public class Main : MonoBehaviour
     {
-        void Start()
+        [SerializeField] RuntimeAnimatorController _animatorController;
+
+        private IAvatarResourceProvider _avatarResourceProvider;
+        private IBinaryDataProvider _binaryDataProvider;
+
+        private AvatarContext _avatarContext;
+
+        async void Start()
         {
-            SceneManager.LoadSceneAsync("CameraSystem_Quest", LoadSceneMode.Additive);
-            SceneManager.LoadSceneAsync("ContentStage_Quest", LoadSceneMode.Additive);
+            await SceneManager.LoadSceneAsync("CameraSystem_Quest", LoadSceneMode.Additive);
+            await SceneManager.LoadSceneAsync("ContentStage_Quest", LoadSceneMode.Additive);
+
+            Initialize();
+
+            var filePath = Path.Combine(Application.streamingAssetsPath, "VRM", "VRoid_Male_PerfectSync.vrm");
+            await _avatarContext.LoadAvatarResourceAsync(filePath);
+        }
+
+        private void Initialize()
+        {
+            _binaryDataProvider = new LocalFileBinaryDataProvider();
+            _avatarResourceProvider = new UrpVrmProvider(_binaryDataProvider);
+            _avatarContext = new AvatarContext(_avatarResourceProvider, _animatorController);
         }
     }
 }

dea1b42aed9d0726abf2769191e25df81e2b8727
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-07-04 22:56:37 +0900

    Add UnityPlayerLooper

---
+++ Crossoverse.HMDApp.Quest/Packages/UnityPlayerLooper/Samples~/LoopCounterSample/Scripts/Lifecycle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityPlayerLooper;
+
+namespace UnityPlayerLooperSamples
+{
+    public class Lifecycle : MonoBehaviour
+    {
+        [SerializeField] LoopCountPresenter _loopCountPresenter;
+
+        LoopCounterA _loopCounterA;
+        LoopCounterB _loopCounterB;
+
+        void Awake()
+        {
+            _loopCounterA = new LoopCounterA();
+            _loopCounterB = new LoopCounterB();
+            _loopCountPresenter.Construct(_loopCounterA, _loopCounterB);
+
+            // Method registeration
+            GlobalPlayerLooper.Register(_loopCounterA.Startup, LoopTiming.PreStartup);
+            GlobalPlayerLooper.Register(_loopCounterA.Tick, LoopTiming.PreBehaviourUpdate);
+
+            // Instance registration
+            GlobalPlayerLooper.Register(_loopCounterB);
+        }
+    }
+}
---
+++ Crossoverse.HMDApp.Quest/Packages/UnityPlayerLooper/Samples~/LoopCounterSample/Scripts/LoopCounterB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityPlayerLooper;
+
+namespace UnityPlayerLooperSamples
+{
+    public class LoopCounterB : IStartable, ITickable, IDisposable
+    {
+        public int LoopCount => _loopCount;
+        private int _loopCount;
+
+        public void Startup()
+        {
+            _loopCount = -1000;
+        }
+
+        public void Tick()
+        {
+            _loopCount++;
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Debug.Log("LoopCounterB.Dispose");
+        }
+    }
+}
